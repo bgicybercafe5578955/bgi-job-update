8a8e6ce5ddaf24d66e793646e4ecb7add202d306
bgicybercafe5578955 <raviwasnik2012@gmail.com>
2026-08-27 22:03:31 +0530

    Fix import paths for database and Job model

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '../../../lib/mongodb';
-import Job from '../../../models/Job';
+import { connectToDatabase } from '../../../../lib/mongodb';
+import Job from '../../../../models/Job';
 
 export async function GET() {
   try {
